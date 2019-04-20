00bc96b4e8058f650e474ba1b0bffe888888f1fc
franklin <frankin@github.com>
2019-04-20 23:55:41 +0800

    black
    add func3

--- algorithm/algorithm-scala/twoSum.scala
+++ algorithm/algorithm-scala/twoSum.scala
@@ -3,6 +3,11 @@ package leetcodeinscala
 import scala.collection.mutable
 
 
+/**
+  * 1 空间换时间，使用hash表避免使用两层for循环嵌套。
+  * 2 不许要定死，可以把value当作是map的key
+  * 3 一次循环可以想办法尽量多做一些事情
+  * */
 object twoSum {
 
   /**
@@ -10,7 +15,6 @@ object twoSum {
     * 暴力法
     * */
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-    var numbers = new mutable.HashSet[Int]
     for(i <- 0 until(nums.length)){
       for(j <- i+1 until(nums.length)){
         if ((nums(i)+nums(j))==target){
@@ -20,15 +24,51 @@ object twoSum {
     }
     throw new IllegalArgumentException("No two sum solution")
   }
+  /**
+    * author black joker
+    * hash table
+    *
+    * scala have option if ruturn some
+    * so that have value you need use .get
+    * if return none so that no value
+    * */
+  def twoSumHashTable(nums: Array[Int], target: Int): Array[Int] = {
+    val map = new mutable.HashMap[Int,Int]()
+    for(i <- 0 until(nums.length)){
+      map.put(nums(i),i)
+    }
+    for(i1 <- 0 to nums.length -1 ){
+      val complement = target - nums(i1)
+      if(map.contains(complement) && map.get(complement) != i1 ) {
+        val ii = map.get(complement)
+        return Array(i1,ii.get)
+      }
+    }
 
-  def twoSum1(): Unit ={
+  }
+  /**
+    * author black joker
+    *
+    * */
 
+
+  def twoSumHashTableOneForeach(nums: Array[Int], target: Int): Array[Int] = {
+    val map = new mutable.HashMap[Int,Int]()
+    for(i <- 0 until(nums.length)){
+      val complement: Int = target - nums(i)
+      if (map.contains(complement)) {
+        return Array(map.get(complement).get, i)
+      }
+
+
+    }
+    throw new IllegalArgumentException("No two sum solution")
   }
 
 
   def main(args: Array[String]): Unit = {
     var myList = Array(1, 2, 13, 5,4)
-    println(twoSum(myList,5))
+    println(twoSumHashTable(myList,5))
 
   }
 }
