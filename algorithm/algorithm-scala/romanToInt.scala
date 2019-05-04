97035ea2121ce5c12cf4869d7e89a1bc44d535c5
franklin <frankin@github.com>
2019-05-04 23:28:52 +0800

    Update romanToInt.scala

--- algorithm/algorithm-scala/romanToInt.scala
+++ algorithm/algorithm-scala/romanToInt.scala
@@ -9,8 +9,28 @@ object romanToInt {
 //    var z = new Array[Int](10)
 
   }
-
-
+/** object Solution {
+    def romanToInt(s: String): Int = {
+      val ss = s.replace("IV","IIII")
+                .replace("IX","VIIII")
+                .replace("XL","XXXX")
+                .replace("XC","LXXXX")
+                .replace("CD","CCCC")
+                .replace("CM","DCCCC")
+      
+      ss.foldLeft(0) {
+        case (i,c) if c == 'I' => i+1
+        case (i,c) if c == 'V' => i+5
+        case (i,c) if c == 'X' => i+10
+        case (i,c) if c == 'L' => i+50
+        case (i,c) if c == 'C' => i+100
+        case (i,c) if c == 'D' => i+500
+        case (i,c) if c == 'M' => i+1000
+        case (i,_) => i+0
+      }
+    }
+}
+**/
 
   def romanToInt(s:String) : Int = {
     var value = 0
